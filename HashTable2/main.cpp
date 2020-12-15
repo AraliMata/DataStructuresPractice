f619109883c056154d5d3e1d79269b16285501b8
Yoceline Mata <56665956+YocelineMata@users.noreply.github.com>
2020-12-15 14:30:30 -0700

    Update main.cpp

--- HashTable2/main.cpp
+++ HashTable2/main.cpp
@@ -2,12 +2,9 @@
 #include "Info.h"
 
 int main(){
-    Info info(707);
-    info.readData("datosOrdenadosHT.txt");
-    //info.printData();
-    info.findIp("10.15.170.220");
-   
-    info.findIp("10.15.170.223");
     
+    Info info(707);
+    info.readData("datosOrdenadosHT.txt");   
+    info.findIp("10.15.170.223");
 }
 
